716fa6f533dd748dc0552792cd68a4a2750a86f6
Carlos-Saenz-Acuna <carlos.ignacio.saenz@gmail.com>
2024-11-10 23:07:06 -0300

    Actualizacion

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-precio = 1599990
+const precio = 1599990
 
 precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+precioSpan.innerHTML = precio;
+
+function sumar() {parrafo = document.querySelector(".cantidad");
+cuenta = Number(parrafo.innerHTML);
+cuenta = cuenta + 1;
+parrafo.innerHTML = cuenta;
+}
+
+function restar() {
+  parrafo = document.querySelector(".cantidad");
+  cuenta = Number(parrafo.innerHTML);
+  cuenta = cuenta - 1;
+  parrafo.innerHTML = cuenta;
+}
+
+cuenta = document.querySelector(".cantidad");
+
+totalSpan = document.querySelector(".valor-total");
+totalSpan.innerHTML = precio * cuenta;
+
